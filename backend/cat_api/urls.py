96eee1454d6cccfa618d89f040509e08dd86f79f
ragav25 <ragavravi25@gmail.com>
2021-01-04 21:08:01 +0530

    initial-setup

---
+++ backend/cat_api/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+# from cat_api.views import PostList, PostDetail
+
+app_name = 'cat_api'
+
+urlpatterns = [
+    # path('<int:pk>/', PostDetail.as_view(), name='detailcreate'),
+    # path('', PostList.as_view, name='listcreate')),
+]
